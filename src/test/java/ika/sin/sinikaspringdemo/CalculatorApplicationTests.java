66e0d0b6df6a14cc6233bc1b03ad8460d42f2f68
irissinka1920 <rospizdyaika@gmail.com>
2024-08-09 22:07:40 +0300

    first attempt

---
+++ src/test/java/ika/sin/sinikaspringdemo/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ika.sin.sinikaspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
