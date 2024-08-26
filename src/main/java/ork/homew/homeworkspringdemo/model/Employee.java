4cbbd60403aad3562b2aea21f8bc521810d5ffce
irissinka1920 <rospizdyaika@gmail.com>
2024-08-26 14:59:48 +0300

    hw Employee 2

--- src/main/java/ork/homew/homeworkspringdemo/model/Employee.java
+++ src/main/java/ork/homew/homeworkspringdemo/model/Employee.java
@@ -1,29 +1,40 @@
 package ork.homew.homeworkspringdemo.model;
 
 import java.util.Objects;
+
 public class Employee {
     private final String name;
     private final String lastName;
-    public Employee(String name, String lastName){
+
+    public Employee(String name, String lastName) {
         this.name = name;
         this.lastName = lastName;
     }
+
     public String getName() {
         return name;
     }
+
     public String getLastName() {
         return lastName;
     }
+
+    public String getFullName(){
+        return name +" " + lastName;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Employee employee)) return false;
         return Objects.equals(name, employee.name) && Objects.equals(lastName, employee.lastName);
     }
+
     @Override
     public int hashCode() {
         return Objects.hash(name, lastName);
     }
+
     @Override
     public String toString() {
         return "Employee{" +
