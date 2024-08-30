ca73085bfcff1d683400ab83b5aaaf892a889e8e
irissinka1920 <rospizdyaika@gmail.com>
2024-08-30 18:58:19 +0300

    H-WStream

---
+++ src/main/java/ork/streamhomew/hwstreamspringdemo/HwStreamSpringDemoApplication.java
@@ -0,0 +1,14 @@
+package ork.streamhomew.hwstreamspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwStreamSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwStreamSpringDemoApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/ork/streamhomew/hwstreamspringdemo/HwStreamSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ork.streamhomew.hwstreamspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwStreamSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
