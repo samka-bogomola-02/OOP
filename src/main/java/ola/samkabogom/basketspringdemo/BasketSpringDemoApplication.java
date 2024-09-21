eb49ac1be8c76425eb158f117736bc1ba6d06e83
Irina Bogomolova <168665027+samka-bogomola-02@users.noreply.github.com>
2024-09-21 17:23:15 +0300

    initial commit

---
+++ src/main/java/ola/samkabogom/basketspringdemo/BasketSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package ola.samkabogom.basketspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketSpringDemoApplication.class, args);
+	}
+
+}
