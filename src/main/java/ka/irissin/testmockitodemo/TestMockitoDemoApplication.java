fc42c8b01b55ddec2a6ef7bb242b1cb565e32aa3
Irina Bogomolova <168665027+samka-bogomola-02@users.noreply.github.com>
2024-10-06 16:56:17 +0300

    initial commit

---
+++ src/main/java/ka/irissin/testmockitodemo/TestMockitoDemoApplication.java
@@ -0,0 +1,13 @@
+package ka.irissin.testmockitodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMockitoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMockitoDemoApplication.class, args);
+	}
+
+}
