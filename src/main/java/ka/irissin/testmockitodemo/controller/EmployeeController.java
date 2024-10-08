036598d9136230b62fbb4edfb34e99dbe4d57f9d
Irina Bogomolova <168665027+samka-bogomola-02@users.noreply.github.com>
2024-10-08 13:27:32 +0300

    first commit

--- src/main/java/ka/irissin/testmockitodemo/controller/EmployeeController.java
+++ src/main/java/ka/irissin/testmockitodemo/controller/EmployeeController.java
@@ -1,1 +1,45 @@
+package ka.irissin.testmockitodemo.controller;
 
+import ka.irissin.testmockitodemo.service.EmployeeService;
+import model.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/add")
+    public Employee add(
+            @RequestParam("firstName") String firstName,
+            @RequestParam("lastName") String lastName,
+            @RequestParam("department") int department,
+            @RequestParam("salary") int salary
+    ) {
+        return employeeService.add(firstName, lastName, department, salary);
+    }
+
+    @GetMapping("/remove")
+    public Employee remove(
+            @RequestParam("firstName") String firstName,
+            @RequestParam("lastName") String lastName
+    ) {
+        return employeeService.remove(firstName, lastName);
+    }
+
+    @GetMapping("/find")
+    public Employee find(
+            @RequestParam("firstName") String firstName,
+            @RequestParam("lastName") String lastName
+    ) {
+        return employeeService.find(firstName, lastName);
+    }
+
+}
--- src/main/java/ka/irissin/testmockitodemo/service/EmployeeService.java
+++ src/main/java/ka/irissin/testmockitodemo/service/EmployeeService.java
@@ -1,1 +1,16 @@
+package ka.irissin.testmockitodemo.service;
 
+import model.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+@Service
+public interface EmployeeService {
+    Employee add (String firstName, String lastName, int department, int salary);
+
+    Employee remove (String firstName, String lastName);
+
+    Employee find (String firstName, String lastName);
+
+    Collection<Employee> getAll();
+}
